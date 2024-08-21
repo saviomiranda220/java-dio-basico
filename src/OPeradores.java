edebcf936a45a1b6cb2bf7e1f29ba098f4d914f7
saviomiranda220 <saviomiranda220@gmail.com>
2024-08-21 16:51:02 -0300

    ATT

---
+++ src/OPeradores.java
@@ -0,0 +1,7 @@
+public class OPeradores {
+    
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem"+ "Java";
+        System.out.println(nomeCompleto);
+    }
+}
